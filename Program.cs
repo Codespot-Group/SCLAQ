af133d9ca7e0f2be2781000a7a9927fda368b419
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2022-05-10 21:19:08 -0300

    Implementation of FileWriter

--- Program.cs
+++ Program.cs
@@ -2,10 +2,8 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(AutoCodeTypeName.generate());
     }
 }
 
 /// 1 - Entender o CREATE TABLE
 /// 2 - Gerar a lista de execução (sequência de inserts)
-/// 3 - Exportar para o arquivo
