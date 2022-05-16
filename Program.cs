c39f6187e572e92abd92ac17ce64e7e913a85607
felipe <berkspar@gmail.com>
2022-05-16 20:05:45 -0300

    add git ignore

--- Program.cs
+++ Program.cs
@@ -2,10 +2,19 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(AutoCodeTypeName.generate());
+        string sql = @"
+            CREATE TABLE Fornecedores (
+                id INT NULL,
+                nome varchar(100) NULL, 
+                cnpj VARCHAR(16) NULL,
+                endereco varchar(100) NULL,
+                telefone varchar(100) NULL,
+                email varchar(100) NULL
+            );
+        ";
+
+        TableModel table = new TableModel(sql);
+
+        System.Console.WriteLine(table.name);
     }
 }
-
-/// 1 - Entender o CREATE TABLE
-/// 2 - Gerar a lista de execução (sequência de inserts)
-/// 3 - Exportar para o arquivo
--- src/models/table_model.cs
+++ src/models/table_model.cs
@@ -1,7 +1,8 @@
+using System.Text.RegularExpressions;
 class TableModel
 {
-    string name;
-    List<AttributeModel> attributes;
+    public string name;
+    public List<AttributeModel> attributes;
 
     public TableModel(string name, List<AttributeModel> attributes)
     {
@@ -9,4 +10,12 @@ class TableModel
         this.attributes = attributes;
     }
 
+    public TableModel(string sql)
+    {
+        Regex tableNameRegex = new Regex("(?<=CREATE TABLE ).+?(?= \\()");
+        Match tableNameMatch = tableNameRegex.Match(sql);
+        this.name = tableNameMatch.ToString();
+        
+        this.attributes = new List<AttributeModel>();
+    }
 }
